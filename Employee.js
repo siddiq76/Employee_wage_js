8226495e02df63a1994262f398540e5e9bd36b9e
“siddiq76” <siddiq.khan0786@gmail.com>
2021-04-16 17:36:34 +0530

    completed

--- Employee.js
+++ Employee.js
@@ -1,11 +1,18 @@
 const wageperhr = 20;
-const hrsinday = 8;
-var employecheck = Math.floor(Math.random() * 2);
-if(employecheck == 1) {
-    console.log("Employee is present");
-}
-else{
-console.log("Employee is absent");
+const IS_FUll_TIME =1;
+const IS_PART_TIME =2;
+var hrsinday 
+var employecheck = Math.floor(Math.random() * 3);
+switch(employecheck) {
+   case IS_FUll_TIME:
+       hrsinday = 8;
+       break;
+   case IS_PART_TIME:
+       hrsinday = 4;
+       break;
+   default:
+       hrsinday = 0;
+       break;
 }
 var dailywage = wageperhr * hrsinday * employecheck;
 console.log("Daily Employee wage :", + dailywage);
