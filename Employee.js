9c3cf6e8eaf8340a8ce1e8967dd6d7631135adf5
“siddiq76” <siddiq.khan0786@gmail.com>
2021-04-17 13:07:52 +0530

    completed UC5

--- Employee.js
+++ Employee.js
@@ -1,7 +1,10 @@
 const wageperhr = 20;
 const IS_FUll_TIME =1;
 const IS_PART_TIME =2;
-var hrsinday 
+const TOTAL_MONTH_DAYS =20;
+var hrsinday;
+var monthlywage =0;
+for(var day = 0; day < TOTAL_MONTH_DAYS; day++){
 var employecheck = Math.floor(Math.random() * 3);
 switch(employecheck) {
    case IS_FUll_TIME:
@@ -14,5 +17,7 @@ switch(employecheck) {
        hrsinday = 0;
        break;
 }
-var dailywage = wageperhr * hrsinday * employecheck;
-console.log("Daily Employee wage :", + dailywage);
+var dailywage = wageperhr * hrsinday;
+monthlywage = monthlywage + dailywage;
+}
+console.log("Monthly Employee wage :", + monthlywage);
